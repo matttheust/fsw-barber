9000e996551cdae7fa47706dbf317eff60d41a08
Matheus Torres <30413727+matttheust@users.noreply.github.com>
2024-08-08 05:20:51 -0300

    feat: ensure barbershop services shown on barbershop page

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -9,6 +9,7 @@ import Image from "next/image"
 import { Button } from "../../_components/ui/button"
 import Link from "next/link"
 import { notFound } from "next/navigation"
+import ServiceItem from "../../_components/service-item"
 interface BarbershopPageProps {
   params: {
     id: string
@@ -18,6 +19,9 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
   const barbershop = await db.barbershop.findUnique({
     where: {
       id: params.id,
+    },
+    include: {
+      services: true,
     },
   })
 
@@ -72,6 +76,17 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
       <div className="space-y-3 border-b border-solid p-5">
         <h2 className="text-xs font-bold uppercase text-gray-400">sobre nós</h2>
         <p className="text-justify text-sm">{barbershop.description}</p>
+      </div>
+
+      {/* SERVIÇOS */}
+
+      <div className="p-5 space-y-3">
+        <h2 className="text-xs font-bold uppercase text-gray-400">serviços</h2>
+        <div className="space-y-3">
+          {barbershop.services.map(service => (
+            <ServiceItem key={service.id} service={service} />
+          ))}
+        </div>
       </div>
     </div>
   )
